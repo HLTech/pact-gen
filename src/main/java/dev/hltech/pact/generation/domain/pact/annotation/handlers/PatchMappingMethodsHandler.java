52c8ee49b871cde8f68d44cac619ca52943a0029
adrian.michalik <adrian.michalik@hltech.com>
2018-09-05 14:54:11 +0200

    Created util for extracting request parameters

--- src/main/java/dev/hltech/pact/generation/domain/pact/annotation/handlers/PatchMappingMethodsHandler.java
+++ src/main/java/dev/hltech/pact/generation/domain/pact/annotation/handlers/PatchMappingMethodsHandler.java
@@ -5,18 +5,16 @@ import dev.hltech.pact.generation.domain.client.model.RequestProperties;
 import dev.hltech.pact.generation.domain.pact.annotation.handlers.util.RawHeadersParser;
 import dev.hltech.pact.generation.domain.pact.annotation.handlers.util.RequestBodyTypeFinder;
 import dev.hltech.pact.generation.domain.pact.annotation.handlers.util.RequestHeaderParamsExtractor;
+import dev.hltech.pact.generation.domain.pact.annotation.handlers.util.RequestParametersExtractor;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ValueConstants;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -36,7 +34,7 @@ public class PatchMappingMethodsHandler implements AnnotatedMethodHandler {
                 method.getAnnotation(PatchMapping.class).headers(),
                 RequestHeaderParamsExtractor.extractRequestHeaderParams(method)))
             .bodyType(RequestBodyTypeFinder.findRequestBodyType(method.getParameters()))
-            .requestParameters(extractRequestParameters(method))
+            .requestParameters(RequestParametersExtractor.extractRequestParameters(method))
             .pathParameters(extractPathParameters(method))
             .build();
     }
@@ -45,47 +43,6 @@ public class PatchMappingMethodsHandler implements AnnotatedMethodHandler {
         return Stream
             .concat(RawHeadersParser.parseHeaders(rawHeaders).stream(), headers.stream())
             .collect(Collectors.toList());
-    }
-
-    private static List<Param> extractRequestParameters(Method feignClientMethod) {
-        return Arrays.stream(feignClientMethod.getParameters())
-            .filter(param -> param.getAnnotation(RequestParam.class) != null)
-            .filter(param -> param.getType() != Map.class)
-            .map(PatchMappingMethodsHandler::extractRequestParameter)
-            .collect(Collectors.toList());
-    }
-
-    private static Param extractRequestParameter(Parameter param) {
-        Param.ParamBuilder builder = Param.builder();
-
-        extractParamDefaultValue(param).ifPresent(builder::defaultValue);
-
-        return builder
-            .name(extractParamName(param))
-            .type(param.getType())
-            .build();
-    }
-
-    private static Optional<Object> extractParamDefaultValue(Parameter param) {
-        RequestParam annotation = param.getAnnotation(RequestParam.class);
-
-        if (annotation.defaultValue().equals(ValueConstants.DEFAULT_NONE) || annotation.defaultValue().isEmpty()) {
-            return Optional.empty();
-        }
-
-        return Optional.of(annotation.defaultValue());
-    }
-
-    private static String extractParamName(Parameter param) {
-        RequestParam annotation = param.getAnnotation(RequestParam.class);
-
-        if (!annotation.name().isEmpty()) {
-            return annotation.name();
-        } else if (!annotation.value().isEmpty()) {
-            return annotation.value();
-        }
-
-        return param.getName();
     }
 
     private static List<Param> extractPathParameters(Method feignClientMethod) {
---
+++ src/main/java/dev/hltech/pact/generation/domain/pact/annotation/handlers/util/RequestParametersExtractor.java
@@ -0,0 +1,59 @@
+package dev.hltech.pact.generation.domain.pact.annotation.handlers.util;
+
+import dev.hltech.pact.generation.domain.client.model.Param;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ValueConstants;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public final class RequestParametersExtractor {
+
+    private RequestParametersExtractor() {}
+
+    public static List<Param> extractRequestParameters(Method feignClientMethod) {
+        return Arrays.stream(feignClientMethod.getParameters())
+            .filter(param -> param.getAnnotation(RequestParam.class) != null)
+            .filter(param -> param.getType() != Map.class)
+            .map(RequestParametersExtractor::extractRequestParameter)
+            .collect(Collectors.toList());
+    }
+
+    private static Param extractRequestParameter(Parameter param) {
+        Param.ParamBuilder builder = Param.builder();
+
+        extractParamDefaultValue(param).ifPresent(builder::defaultValue);
+
+        return builder
+            .name(extractParamName(param))
+            .type(param.getType())
+            .build();
+    }
+
+    private static Optional<Object> extractParamDefaultValue(Parameter param) {
+        RequestParam annotation = param.getAnnotation(RequestParam.class);
+
+        if (annotation.defaultValue().equals(ValueConstants.DEFAULT_NONE) || annotation.defaultValue().isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(annotation.defaultValue());
+    }
+
+    private static String extractParamName(Parameter param) {
+        RequestParam annotation = param.getAnnotation(RequestParam.class);
+
+        if (!annotation.name().isEmpty()) {
+            return annotation.name();
+        } else if (!annotation.value().isEmpty()) {
+            return annotation.value();
+        }
+
+        return param.getName();
+    }
+}
