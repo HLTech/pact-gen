5f7ef2808796ead64a157264cec0d5ceca3d74ea
Filip Łazarski <filip.lazarski@hltech.com>
2018-09-13 12:09:07 +0200

    ResponseInfo annotation can be used multiple times on the same method

--- src/main/java/dev/hltech/pact/generation/domain/client/feign/ResponseInfo.java
+++ src/main/java/dev/hltech/pact/generation/domain/client/feign/ResponseInfo.java
@@ -2,13 +2,15 @@ package dev.hltech.pact.generation.domain.client.feign;
 
 import org.springframework.http.HttpStatus;
 
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Repeatable(ResponsesInfo.class)
 public @interface ResponseInfo {
 
     HttpStatus status();
