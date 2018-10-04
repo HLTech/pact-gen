91e1783664df5d256be8fb01b8de4be6052648c4
Filip Łazarski <filip.lazarski@hltech.com>
2018-10-04 09:43:32 +0200

    Multiple InteractionInfo put into InteractionsInfo

--- src/test/unit/dev/hltech/pact/generation/domain/client/feign/sample/ResponseInfoFeignClient.java
+++ src/test/unit/dev/hltech/pact/generation/domain/client/feign/sample/ResponseInfoFeignClient.java
@@ -1,6 +1,7 @@
 package dev.hltech.pact.generation.domain.client.feign.sample;
 
 import dev.hltech.pact.generation.domain.client.feign.InteractionInfo;
+import dev.hltech.pact.generation.domain.client.feign.InteractionsInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface ResponseInfoFeignClient {
 
     @GetMapping(path = "/")
-    @InteractionInfo(responseStatus = HttpStatus.OK)
-    @InteractionInfo(responseStatus = HttpStatus.ACCEPTED)
+    @InteractionsInfo({
+            @InteractionInfo(responseStatus = HttpStatus.OK),
+            @InteractionInfo(responseStatus = HttpStatus.ACCEPTED)
+    })
     void getTestObject();
 }
