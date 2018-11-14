2f67af75781da4a1d65036440924d1944e09dab5
Filip Łazarski <filip.lazarski@hltech.com>
2018-11-14 10:34:43 +0100

    TypeExtractor minor refactoring

--- src/test/groovy/com/hltech/pact/gen/domain/client/util/TypeExtractorSpec.groovy
+++ src/test/groovy/com/hltech/pact/gen/domain/client/util/TypeExtractorSpec.groovy
@@ -4,7 +4,6 @@ package com.hltech.pact.gen.domain.client.util
 import com.hltech.pact.gen.domain.GenericResponseType
 import com.hltech.pact.gen.domain.TestParam
 import spock.lang.Specification
-import spock.lang.Subject
 import spock.lang.Unroll
 
 import java.lang.reflect.Type
@@ -12,18 +11,20 @@ import java.lang.reflect.Type
 @Unroll
 class TypeExtractorSpec extends Specification {
 
-    @Subject
-    def parameterTypeExtractor = new TypeExtractor()
-
     def "should correctly extract parameter type"(Type type, List<Class<?>> clazz) {
         expect:
-            parameterTypeExtractor.extractParameterTypesFromType(type) == clazz
+            TypeExtractor.extractParameterTypesFromType(type) == clazz
 
         where:
             type << TypeExtractorSpec.methods.find {it.name == 'testMethod'}.getParameters().collect { param ->
                 param.getParameterizedType()
             }
             clazz << [[], [], [], [Long], [TestParam, String], [int]]
+    }
+
+    def "should return empty list for null type"() {
+        expect:
+            TypeExtractor.extractParameterTypesFromType(null).size() == 0
     }
 
     static void testMethod(String string,
