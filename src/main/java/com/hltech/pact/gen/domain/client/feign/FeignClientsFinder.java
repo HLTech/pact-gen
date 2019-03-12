ff470b18e972f9fd394ef78a33814a688d666c91
Filip Łazarski <filip.lazarski@hltech.com>
2019-03-12 18:02:54 +0100

    Possibility to hide feign clients from Pact Gen scanning

--- src/main/java/com/hltech/pact/gen/domain/client/feign/FeignClientsFinder.java
+++ src/main/java/com/hltech/pact/gen/domain/client/feign/FeignClientsFinder.java
@@ -4,10 +4,13 @@ import org.reflections.Reflections;
 import org.springframework.cloud.openfeign.FeignClient;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class FeignClientsFinder {
 
     public Set<Class<?>> findFeignClients(String packageRoot) {
-        return new Reflections(packageRoot).getTypesAnnotatedWith(FeignClient.class);
+        return new Reflections(packageRoot).getTypesAnnotatedWith(FeignClient.class).stream()
+            .filter(feignClient -> !feignClient.isAnnotationPresent(ExcludeFeignClient.class))
+            .collect(Collectors.toSet());
     }
 }
