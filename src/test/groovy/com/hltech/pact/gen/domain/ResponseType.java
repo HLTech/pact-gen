7dade64bd2031e2129d158f0ec71a6200fb8f48f
Filip Łazarski <filip.lazarski@hltech.com>
2019-04-17 12:49:33 +0200

    Test for array in response body

--- src/test/groovy/com/hltech/pact/gen/domain/ResponseType.java
+++ src/test/groovy/com/hltech/pact/gen/domain/ResponseType.java
@@ -6,10 +6,13 @@ public class ResponseType {
 
     private final String responseFoo;
     private final String responseBar;
+    private final int[] intArray;
 
-    public ResponseType(String responseFoo, @JsonProperty(defaultValue = "responseReplacedBar") String responseBar) {
+    public ResponseType(
+        String responseFoo, @JsonProperty(defaultValue = "responseReplacedBar") String responseBar, int[] intArray) {
         this.responseFoo = responseFoo;
         this.responseBar = responseBar;
+        this.intArray = intArray;
     }
 
     public String getResponseFoo() {
@@ -18,5 +21,9 @@ public class ResponseType {
 
     public String getResponseBar() {
         return responseBar;
+    }
+
+    public int[] getIntArray() {
+        return intArray;
     }
 }
