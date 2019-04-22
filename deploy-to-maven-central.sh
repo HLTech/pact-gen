156fff0e12a618ea77f376d6cbe18a5ea331379d
garlicsauce <adrian.michalik@wp.pl>
2019-04-22 20:13:52 +0200

    Updated maven-central-deployment script

--- deploy-to-maven-central.sh
+++ deploy-to-maven-central.sh
@@ -1,8 +1,8 @@
 echo "decrypting secring.gpg..."
 openssl aes-256-cbc -K $encrypted_12c8071d2874_key -iv $encrypted_12c8071d2874_iv -in $TRAVIS_BUILD_DIR/secring.gpg.enc -out $TRAVIS_BUILD_DIR/secring.gpg -d
 
-echo "uploadArchives..."
-./gradlew uploadArchives -Psigning.keyId=$signingKeyId -Psigning.password=$secretKeyPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg --info --stacktrace
+echo "publish archives..."
+./gradlew publish -Psigning.keyId=$signingKeyId -Psigning.password=$secretKeyPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg --info --stacktrace
 
 echo "closeAndPromoteRepository..."
 ./gradlew closeAndReleaseRepository --info
