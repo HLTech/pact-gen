cfe1580742df287f472bb78c4b0f3f2781a71c0b
Robert Wojcik <Robert.Wojcik@hltech.com>
2020-03-27 17:42:55 +0100

    Removed usage of deprecated methods

--- src/main/java/com/hltech/pact/gen/domain/client/annotation/HandlersFactory.java
+++ src/main/java/com/hltech/pact/gen/domain/client/annotation/HandlersFactory.java
@@ -1,8 +1,8 @@
 package com.hltech.pact.gen.domain.client.annotation;
 
-import com.google.common.collect.Lists;
 import com.hltech.pact.gen.domain.client.annotation.handlers.AnnotatedMethodHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HandlersFactory {
@@ -10,7 +10,7 @@ public class HandlersFactory {
     List<AnnotatedMethodHandler> createHandlers(List<Class<?>> classes)
         throws IllegalAccessException, InstantiationException {
 
-        List<AnnotatedMethodHandler> result = Lists.newArrayList();
+        List<AnnotatedMethodHandler> result = new ArrayList<>();
 
         for (Class<?> handlerClass : classes) {
             result.add((AnnotatedMethodHandler) handlerClass.newInstance());
