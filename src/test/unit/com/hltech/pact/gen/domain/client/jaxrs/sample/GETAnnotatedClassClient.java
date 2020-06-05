d2f4d6f4bf2df6bee8863de6a658c4a7aacaf57b
Robert Wojcik <Robert.Wojcik@hltech.com>
2020-06-05 19:41:37 +0200

    Added JAX-RS client finder

---
+++ src/test/unit/com/hltech/pact/gen/domain/client/jaxrs/sample/GETAnnotatedClassClient.java
@@ -0,0 +1,25 @@
+package com.hltech.pact.gen.domain.client.jaxrs.sample;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+public class GETAnnotatedClassClient {
+
+    @GET
+    @Path(value = "/testPathGet/{pathParamK}")
+    @Consumes(value = "")
+    public Response httpGetResponseAccepted(@HeaderParam("Header-A") String headerParamA,
+                                            @HeaderParam("Header-B") String headerParamB,
+                                            @DefaultValue (value = "default-value") @QueryParam("Param-Q") String queryParamQ,
+                                            @QueryParam("Param-W") Integer queryParamW,
+                                            @PathParam("pathParamK") String pathParamK,
+                                            String body) {
+        return Response.accepted().build();
+    }
+}
---
+++ src/test/unit/com/hltech/pact/gen/domain/client/jaxrs/sample/GETAnnotatedMethodClient.java
@@ -0,0 +1,10 @@
+package com.hltech.pact.gen.domain.client.jaxrs.sample;
+
+import javax.ws.rs.Path;
+
+
+public interface GETAnnotatedMethodClient {
+
+    @Path("/class")
+    String getTestObject();
+}
