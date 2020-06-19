9ab49e9bfc65075eea7289da23e3d9eef6de56f5
Pawel Wlodarczyk <pawel.wlodarczyk@hltech.com>
2020-06-19 22:37:17 +0200

    Moved ResponseRepresentation creation to static classes

--- src/main/java/com/hltech/pact/gen/domain/client/feign/FeignMethodRepresentationExtractor.java
+++ src/main/java/com/hltech/pact/gen/domain/client/feign/FeignMethodRepresentationExtractor.java
@@ -3,15 +3,11 @@ package com.hltech.pact.gen.domain.client.feign;
 import com.google.common.collect.Lists;
 import com.hltech.pact.gen.domain.client.ClientMethodRepresentationExtractor;
 import com.hltech.pact.gen.domain.client.annotation.handlers.AnnotatedMethodHandler;
-import com.hltech.pact.gen.domain.client.model.Body;
 import com.hltech.pact.gen.domain.client.model.ClientMethodRepresentation;
-import com.hltech.pact.gen.domain.client.model.Param;
 import com.hltech.pact.gen.domain.client.model.RequestRepresentation;
 import com.hltech.pact.gen.domain.client.model.ResponseRepresentation;
 import com.hltech.pact.gen.domain.client.util.RawHeadersParser;
-import com.hltech.pact.gen.domain.client.util.TypeExtractor;
 import org.apache.commons.lang3.ArrayUtils;
-import org.springframework.http.HttpStatus;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -50,7 +46,7 @@ public class FeignMethodRepresentationExtractor implements ClientMethodRepresent
 
         List<ResponseRepresentation> results = Arrays
             .stream(method.getDeclaredAnnotationsByType(InteractionInfo.class))
-            .map(annotation -> populateResponse(
+            .map(annotation -> ResponseRepresentation.from(
                 annotation.responseStatus(),
                 RawHeadersParser.parseAll(ArrayUtils.addAll(annotation.responseHeaders(), responseHeaders)),
                 method,
@@ -60,26 +56,6 @@ public class FeignMethodRepresentationExtractor implements ClientMethodRepresent
 
         return !results.isEmpty()
             ? results
-            : Lists.newArrayList(
-                populateResponse(HttpStatus.OK, RawHeadersParser.parseAll(responseHeaders), method, "", false));
-    }
-
-    private static ResponseRepresentation populateResponse(HttpStatus status,
-                                                           List<Param> headers,
-                                                           Method feignClientMethod,
-                                                           String description,
-                                                           boolean isEmptyBodyExpected) {
-
-        return ResponseRepresentation.builder()
-            .status(status)
-            .headers(headers)
-            .body(Body.builder()
-                .type(feignClientMethod.getReturnType())
-                .genericArgumentTypes(
-                    TypeExtractor.extractParameterTypesFromType(feignClientMethod.getGenericReturnType()))
-                .build())
-            .description(description)
-            .emptyBodyExpected(isEmptyBodyExpected)
-            .build();
+            : Lists.newArrayList(ResponseRepresentation.getDefaultForMethod(method, responseHeaders));
     }
 }
--- src/main/java/com/hltech/pact/gen/domain/client/model/ResponseRepresentation.java
+++ src/main/java/com/hltech/pact/gen/domain/client/model/ResponseRepresentation.java
@@ -1,9 +1,12 @@
 package com.hltech.pact.gen.domain.client.model;
 
+import com.hltech.pact.gen.domain.client.util.RawHeadersParser;
+import com.hltech.pact.gen.domain.client.util.TypeExtractor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 @Data
@@ -15,4 +18,32 @@ public class ResponseRepresentation {
     private final Body body;
     private final String description;
     private final boolean emptyBodyExpected;
+
+    public static ResponseRepresentation getDefaultForMethod(Method method, String[] responseHeaders) {
+        return ResponseRepresentation.from(
+            HttpStatus.OK,
+            RawHeadersParser.parseAll(responseHeaders),
+            method,
+            "",
+            false);
+    }
+
+    public static ResponseRepresentation from(HttpStatus status,
+                                                          List<Param> headers,
+                                                          Method method,
+                                                          String description,
+                                                          boolean isEmptyBodyExpected) {
+
+        return ResponseRepresentation.builder()
+            .status(status)
+            .headers(headers)
+            .body(Body.builder()
+                .type(method.getReturnType())
+                .genericArgumentTypes(
+                    TypeExtractor.extractParameterTypesFromType(method.getGenericReturnType()))
+                .build())
+            .description(description)
+            .emptyBodyExpected(isEmptyBodyExpected)
+            .build();
+    }
 }
