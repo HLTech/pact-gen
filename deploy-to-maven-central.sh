7110f8bdd32a4af5daf9035fddcc08c04ec5a6d0
wlodarcp <Pan.Wlodarczyk.Pawel@gmail.com>
2021-07-31 09:00:07 +0200

    Migrated from travis to github actions added release

--- deploy-to-maven-central.sh
+++
@@ -1,5 +0,0 @@
-echo "decrypting secring.gpg..."
-openssl aes-256-cbc -K $encrypted_12c8071d2874_key -iv $encrypted_12c8071d2874_iv -in $TRAVIS_BUILD_DIR/secring.gpg.enc -out $TRAVIS_BUILD_DIR/secring.gpg -d
-
-echo "publish archives & closeAndPromoteRepository..."
-./gradlew build publishToNexus closeAndReleaseRepository -Psigning.keyId=$signingKeyId -Psigning.password=$secretKeyPassword -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg --info --stacktrace
---
+++ deploy_to_maven_central.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+function cleanup {
+    echo "🧹 Cleanup..."
+    rm -f gradle.properties secring.gpg
+}
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+echo "🚀 Preparing to deploy..."
+
+echo "🔑 Decrypting files..."
+gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
+    --output secring.gpg secring.gpg.enc
+
+gpg --quiet --batch --yes --decrypt --passphrase="${GPG_SECRET}" \
+    --output gradle.properties gradle.properties.enc
+
+gpg --fast-import --no-tty --batch --yes secring.gpg
+
+echo "📦 Publishing..."
+
+./gradlew currentVersion publishToSonatype closeAndReleaseSonatypeStagingRepository
+
+echo "✅ Done!"
